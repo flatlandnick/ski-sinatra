dd89b8abba33c13021df43a87e06abf572eb440e
flatlandnick <nicholas.lunn@flatironschool.com>
2020-12-11 10:20:06 -0600

    update environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,14 @@
-@environment = ENV['SINATRA_ENV'] ||= "development"
+# @environment = 
+ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, @environment)
 
-@dbconfig = YAML.load(File.read('config/database.yml'))
+# @dbconfig = YAML.load(File.read('config/database.yml'))
 
-ActiveRecord::Base.establish_connection(@dbconfig[@environment])
+# ActiveRecord::Base.establish_connection(@dbconfig[@environment])
+
+set :database_file, './database.yml'
 
 require './app/controllers/application_controller'
 require_all 'app'
